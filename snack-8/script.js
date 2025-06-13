c1a70a2e1b88474f14752383ff7758f5b192f37c
FrancescoCellitti <francescocellitti@virgilio.it>
2025-06-13 14:38:16 +0200

    snacks 8

--- snack-8/script.js
+++ snack-8/script.js
@@ -21,3 +21,13 @@ const students = [
 
 // Recupera la classe dello studente 'Marco Lanci'
 // Risultato: '3C'
+let classe = ''
+students.forEach((nome) => {
+  if (nome.name === "Marco Lanci") {
+    classe = nome.class
+  }
+})
+
+const nome = students.filter(names => names.name === "Marco Lanci")
+console.log(nome[0].class)
+console.log(classe)
