6f1ba45ab29c3e1a1eb0f7868514cd22e9d43a7f
FrancescoCellitti <francescocellitti@virgilio.it>
2025-06-13 14:06:48 +0200

    snacks 3

--- snack-3/script.js
+++ snack-3/script.js
@@ -2,3 +2,12 @@ const numbers = [2, 8, 4, 7, 2, 87];
 
 // Crea un nuovo array in cui tutti i numeri siano incrementati di 1
 // Risultato: [3, 9, 5, 8, 3, 88]
+const increment = []
+numbers.forEach((numeri) => {
+    increment.push(numeri + 1)
+})
+console.log(increment)
+
+
+const incremento = numbers.map(numero => numero + 1)
+console.log(incremento)
