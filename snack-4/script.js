e5a8ea8392535cae9e21a2742d236d4e477d7e57
FrancescoCellitti <francescocellitti@virgilio.it>
2025-06-13 14:15:03 +0200

    snacks 4

--- snack-4/script.js
+++ snack-4/script.js
@@ -7,3 +7,6 @@ const posts = [
 
 // Crea un nuovo array coi nomi di tutti gli autori
 // Risultato: ['Marco', 'Luca', 'Fabrizio', 'Enrico']
+
+const Autori = posts.map( nome => nome.author)
+console.log(Autori)
